b79c81ab3f0d5718dc0be7137f81a06401c31c6d
kailsh <kailshkumartkg@gmail.com>
2023-07-27 00:30:06 +0530

    initial commit

---
+++ even_and_odd_num.js
@@ -0,0 +1,234 @@
+//  var readline =require('readline-sync')
+// var num =readline.questionInt('enter num--:')
+// var i=1
+// do{
+//     if(i%2==0){
+//         console.log(i,'even')
+//     }
+//     else{
+//         console.log(i,'odd')
+//     }
+//     i=i+1
+// }
+// while(i<=num)
+
+
+// var readline =require('readline-sync')
+// var num =readline.questionInt('enter num--:')
+// for(var i=1; i<=num; i++;){
+//     if(i%2==0){
+//     console.log(i,"even")
+// }
+// else{
+//     console.log(i,"odd")
+// }
+
+// for(i=1; i<=50;  i++){
+//     if(i%2==0){
+//         console.log(i'even')
+//     }
+//     else{
+//         console.log(i'odd')
+//     }
+// }
+
+// var even_count=0
+// var odd_count=0
+// for(var i=1; i<=50; i++){
+//     if(i%2==0){
+//         even_count=even_count+1
+//     }
+//     else {
+//         odd_count=odd_count+1
+//     }
+// }
+// console.log('even_count=====',even_count)
+// console.log('odd_count=====',odd_count)
+
+
+
+
+//       var readlineaSync =require("readline-sync")
+// var num =readlineaSync.questionInt("enter the num====")
+// var num1 =readlineaSync.questionInt("enter the num1====")
+// while(num<num1){
+//     if(num<=0 && num%2!=0){
+//         oddNumber=oddNumber+num;
+//     }
+//     else if(num%2==0){
+//         evenNumber=evenNumber+num;
+//     }
+//     num=num++;
+// }
+// console.log("total sum of the odd number is",oddNumber);
+// console.log("total sum of the even number is",evenNumber);
+
+
+
+
+// var readline =require('readline-sync')
+// var num =readline.questionInt("enter the num===")
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even hai")
+//         num=num+i
+//     }
+//     else if(i%2!=0){
+//         console.log(i,"odd hai")
+
+//     }
+//     i=i+1
+// }
+// console.log("total sum of even number")
+// console.log("total sum of odd number")
+
+
+// var readline =require("readline-sync")
+// let number =readline.questionInt ("Enter a number====");
+// var evenSum=0
+// var oddSum=0
+// var i=1
+
+// while (i<=number){
+//   if (i % 2 == 0){
+//     evenSum=evenSum+i
+//   } 
+// else {
+//     oddSum=oddSum+i
+
+//   }
+//   i++
+// }
+// console.log("Sum of even numbers:", evenSum);
+// console.log("Sum of odd numbers:", oddSum);
+
+// var num=20
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even hai")
+//     }
+//     else{
+//         console.log(i,"odd hai")
+//     }
+//     i=i+1
+// }
+
+// var rlSync =require("readline-sync")
+// var num =rlSync.questionInt("enter any num=====")
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even number")
+//     }
+//     else{
+//         console.log(i,"odd number")
+//     }
+//     i=i+1
+// }
+
+
+
+
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num===")
+// var i=2
+// count =0
+// while(i<=num){
+//     if(num%i==0){
+//         count=count+1
+//     }
+//     i=i+1
+// }
+// console.log("count-----",count)
+// if(count==1){
+//     console.log("it is a prime number")
+// }
+// else{
+//     console.log("it is a not prime number")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num====")
+// fact =1
+// while(num>0){
+//     fact=fact*num
+//     num=num-1
+// }
+// console.log("fact")
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num====")
+// var i=0
+// while(i<=num){
+//     var num1=readline.questionInt("enter any num1===")
+//     if(num1>0){
+//         console.log(num1,"postive number")
+//     }
+//     else{
+//         console.log(num1,"neagtive number")
+//     }
+//     i=i+1
+// }
+
+// var rlSync =require("readline-sync")
+// var num =rlSync.questionInt("enter any num===")
+// var i=0
+// var sum=0
+// while(i<num){
+//     if(num%i==0){
+//         sum=sum+i
+//     }
+//     i=i+1
+// }
+// if(sum==num){
+//     console.log(sum,"it is a perfect number")
+// }
+// else{
+//     console.log(sum,"it is not perfect number")
+// }
+
+
+// var Name ="komal"
+// var name ="swati"
+// console.log(Name+name)
+
+
+// var num_12 =24;
+// console.log(num_12)
+
+// var string1 ="i am staying in the \"sarjapur campus \""
+// console.log(string1)
+
+// var myFavplace ='my favourite place is "paris"';
+// console.log(myFavplace)
+
+// var name ="navgarukul"
+// var num =29;
+// var result =name+num;
+// console.log(result)
+
+
+// var num =24
+// var a ="kumar"
+// console.log(24/"kumar")
+
+// var b="Hello this is my ‘Dell Laptop’ ";
+// console.log(b);
+
+// var a = 25;        
+// var b = 80.5;       
+// var c = -2; 
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+// console.log("Nav Gurukul" / 2);       // Output: NaN
+// console.log("Dharamshala" / 2 + 10);  // Output: NaN
+// console.log(Math.sqrt(-1));         // Output: NaN
+
+
+
+
---
+++ meraki_learn.js
@@ -0,0 +1,674 @@
+
+// num1=19.3;
+// console.log(num1)
+
+
+// num1=5;
+// var num2=6;
+// console.log(num2>num1);
+
+
+// var a = 3;
+// var b = 5;
+// console.log(b
+
+
+// var a = 3;
+// var b = 5;
+// console.log(!=a);
+
+
+// var a;
+// var b = "Hello World!";
+// console.log(a); 
+// console.log(b)
+
+// var z;
+// console.log(z);
+
+
+// var n;
+// console.log(n);
+
+// var a = null;
+// console.log(a); 
+// var b = "Hello World!"
+// console.log(b); 
+// b = null;
+// console.log(b)
+
+// var y= null;
+// console.log(y);
+
+
+// var val = null;
+// console.log(val);
+
+
+// var empty = null;
+// console.log(empty); 
+// console.log(typeof(empty)); 
+
+
+// var notDefined;
+// console.log(notDefined); 
+// console.log(typeof(notDefined)); 
+
+
+// var emptyObject = {};
+
+// var person = {"name": "Nayak", "surname": "Kumar", "age": "24"};
+
+// // For better reading
+// var scooty = {
+// "modal": "Ampere Rio",
+// "color": "white",
+// "Speed": "35 km"
+// }
+
+
+// var myDetails={
+//     firstName:"kailash",
+//     lastName:"singh",
+//     phoneNumber:"7033479013",
+//     isHealthy:false
+//     }
+//     console.log(myDetails);
+
+
+// var obj1 = { a: 5, b: 6 };
+// obj1["a"] =7;
+// console.log(obj1)
+
+
+// var myObj = { name: "kailash", height: 5.4 };
+// myObj[name] =7;
+// console.log(myObj)
+
+// var languages = ["Telugu", "Tamil", "Kannada"];
+// var cities = ["Dharamshala", "Bangalore", "Pune"];
+// console.log(cities[0]);  
+// console.log(cities[1]);   
+
+// var languages =["Hindi", "English", "Bengoli"];
+// var cities =["Siliguri", "Thakurganj", "Kishanganj"];
+// console.log("languages[0]");
+// console.log("language[2]");
+
+
+// var arr1=[2,4,6,8];
+// console.log(arr1);
+
+// var arr1= [1, 'HI', 3, 12.3];
+// arr1[1] =4;
+// console.log(arr1);
+
+// var arr1= ["kumar", 24, "Academic Associate"];
+// arr1['0'] ="Nayak";
+// console.log(arr1);
+
+
+// var day = "friday".toLowerCase();
+
+// if (day == "monday") {
+//   console.log("Dhanwantari\nSiddhi\Kritika\Salon\nMayuri\nGauri");
+// }
+// else if (day == "tuesday") {
+//   console.log("Keemaya\nRoshni\nPooja\Priyanka\nnalini\nManisha");
+// }
+// else if (day == "wednesday") {
+//   console.log("Shrusti\Karuna\Parveen\Sandhya\nLeena\nKarishma");
+// }
+// else if (day == "thursday") {
+//   console.log("Nikita\nMona\nPriyanka\Tejashree\nLovely\nMehzebin");
+// }
+// else if (day == "friday") {
+//   console.log("Pihu\Sejal\nChaya\Shrestha\nMonali\nAardhangya");
+// }
+// else if (day == "saturday") {
+//   console.log("Gunjan\nShweta\nRani\nIsha\nKusu\Madhu\n");
+// }
+// else if (day == "sunday") {
+//   console.log("Swati\nKhusboo\nidhi\Preeti\nDipti\nAnamika");
+// }
+// else {
+//   console.log("Please enter valid week day")
+// }
+
+
+// var x = 10;
+// var y = 12
+// var z = 90
+ 
+// if (x > y){
+//   if (x < z){
+//       x = y
+//   }
+//   else{
+//       m = z
+//   }
+// }
+// else {
+//   if(y > z){
+//       m = y
+//   }
+//   else {
+//       m = x
+//   }
+   
+// }
+// console.log(m)
+
+
+// var time = 6.9;
+// if (time >= 5 && time <= 6) {
+//  console.log("sleep well");
+// }
+// else if (time >= 6 && time <= 7) {
+//  console.log("Morning Exercise");
+// }
+// else if(time > 7 && time < 8.30){
+//  console.log("Break+Breakfast");
+// }
+// else if ( time > 8.30 && time < 12){
+//  console.log("Study");
+// }
+
+
+// let readlineSync = require("readline-sync");
+// var number=readlineSync.questionInt("enter a number===");
+// if (number<10){
+//    console.log("num is less than 10");
+// }
+// else if(number>10 && number<20){
+//    console.log("num is less than 20");
+// }
+// else{
+//    console.log("greater than 20");
+// }
+
+
+// var num1 = 300 
+// let readlineSync = require("readline-sync");
+// var num2=readlineSync.question("enter a number==");
+// if(num1==num2){
+//    console.log("equal hai")
+// }
+// else{
+//    console.log("not equal")
+// }
+
+
+// var number1 = 44 * 30;
+// let readlineSync = require("readline-sync");
+// var number2=readlineSync.questionInt("enter a number===");
+// if(number2>=number2){
+//    console.log("number2 is greater than or equal");
+// }
+// else{
+//    console.log("number2 is lesser");
+// }
+
+
+// let readlineSync = require("readline-sync");
+// var water=readlineSync.questionInt("enter water level==");
+// if (water<1){
+//    console.log("more water needs to fill");
+// }
+// else if (water>=1 && water<=10){
+//    console.log("no need to fill water");
+// }
+// else{
+//    console.log("overflow");
+// }
+
+
+// let readlineSync = require("readline-sync");
+// var varx=readlineSync.questionInt("enter a number")
+// if(varx%2===0){
+//    console.log("divisible hai")
+// }
+// else{
+//    console.log("not divisible")
+// }
+
+
+
+// let readlineaSync = require("readline-sync");
+// var varX=readlineaSync.questionInt("enter a number===");
+// var vary=readlineSync.questionInt("enter a number===");
+// if(varX%vary===0){
+//    console.log("Divisible");
+// }
+// else{
+//    console.log("Not divisible");
+// }
+
+
+
+// let readlineaSync = require("readline-sync");
+// var number1=readlineaSync.questionInt("enter a number===");
+// var number2=readlineaSync.questionInt("enter a number===");
+// if(number1>number2){
+//    console.log("number1 is greater");
+// }
+// else{
+//    console.log("number2 is greater");
+// }
+
+
+// let readlineaSync = require("readline-sync");
+// var number=readlineaSync.questionInt("enter a number===");
+// if(number%5===0 && number%15===0){
+//    console.log("Divisible by both");
+// }
+// else{
+//    console.log("Not divisible by both");
+// }
+
+// let readlineaSync = require("readline-sync");
+// var age=readlineaSync.questionInt("enter age===");
+// if(age>=5){
+//    console.log("You can go school");
+// }
+// if(age>=18){
+//    console.log("You can vote in elections.");
+// }
+// if(age>=21){
+//    console.log("You can drive a car");
+// } 
+// if(age>=24){
+//    console.log("You can marry");
+// }
+// if(age>=25){
+//    console.log("You can drink");
+// }
+
+
+// var i=0
+// while(i<=10){
+//     console.log(i)
+//     i=i+1
+// }
+
+// var num=10
+// var i=1
+// while(i<=num){
+//     console.log(i)
+//     i=i+1
+// }
+// do{
+//     console.log(i)
+//     i=i+1
+// }
+// while(i<=num)
+
+// var i=10
+// var num=0
+// while(i>=num){
+//     console.log(i)
+//     i=i-1
+// }
+// do{
+//     console.log(i)
+//     i=i-1
+// }
+// while(i>=num)
+
+// v i=1ar num=10
+// var
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i+1
+// }
+
+// do{
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i+1
+// }
+// while(i<=num)
+// var evenSum=0
+// var oddSum=0
+// while(i<=num){
+//     if(i%2==0){
+//         evenSum=evenSum+i
+//     }
+//     else{
+//         oddSum=oddSum+i
+//     }
+//     i=i+1
+// }
+// console.log("total of evensum",evenSum)
+// console.log("total of oddsum",oddSum)
+
+// do{
+//     if(i%2==0){
+//         evenSum=evenSum+i
+//     }
+//     else{
+//         oddSum=oddSum+i
+//     }
+//     i=i+1
+// }
+// while(i<=num)
+// console.log("total of evensum",evenSum)
+// console.log("total of oddsum",oddSum)
+
+
+// var i=10
+// var num=1
+// while(i>=num){
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i-1
+// }
+
+// do{
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i-1
+// }
+// while(i>=num)
+
+// var evensum=0
+// var oddsum=0
+// while(i>=num){
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i-1
+// }
+// console.log("total of evensum",evensum)
+// console.log("total of oddsum",oddsum)
+// do{
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i-1
+// }
+// while(i>=num)
+//     console.log("total of evensum",evensum)
+//     console.log("total of oddsum",oddsum)
+
+// var i=25
+// var num=1
+// var evencout=0
+// var oddcount=0
+// while(i>=num){
+//     if(i%2==0){
+//         evencout=evencout+1
+//     }
+//     else{
+//         oddcount=oddcount+1
+//     }
+//     i=i-1
+// }
+// console.log("total of evencount",evencout)
+// console.log("total of oddcount",oddcount)
+
+// do{
+//     if(i%2==0){
+//         evencout=evencout+1
+//     }
+//     else{
+//         oddcount=oddcount+1
+//     }
+//     i=i-1
+// }
+// while(i>=num)
+// console.log("total of evencount",evencout)
+// console.log("total of oddcount",oddcount)
+
+
+// var num=15
+// var i=1
+// var evensum=0
+// while(i<=num){
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     i=i+1
+// }
+// console.log("total of evensum",evensum)
+
+
+// var readline =require("readline-sync")
+// var num=readline.questionInt("enter the any num======")
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i+1
+// }
+
+// do{
+//     if(i%2==0){
+//         console.log(i,"even")
+//     }
+//     else{
+//         console.log(i,"odd")
+//     }
+//     i=i+1
+// }
+// while(i<=num)
+
+// var readline =require("readline-sync")
+// var num=readline.questionInt("enter the any num========")
+// var i=10
+// var evensum=0
+// var oddsum=0
+// while(i<=num){
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i+1
+// }
+// console.log("total of evensum",evensum)
+// console.log("total of oddsum",oddsum)
+// do{
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i+1
+// }
+// while(i<=num)
+// console.log("total of evensum",evensum)
+// console.log("total of oddsum",oddsum)
+
+// while(i>=num){
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i-1
+// }
+// console.log("total of evensum",evensum)
+// console.log("total of oddsum",oddsum)
+
+
+// do{
+//     if(i%2==0){
+//         evensum=evensum+i
+//     }
+//     else{
+//         oddsum=oddsum+i
+//     }
+//     i=i-1
+// }
+// while(i>=num)
+// console.log("total of evensum",evensum)
+// console.log("total of oddsum",oddsum)
+
+// var num=10
+// var i=0
+// var sum=0
+// while(i<=num){
+//     sum=sum+i
+//     i=i+1
+// }
+// console.log("total of sum",sum)
+
+// do{
+//     sum=sum+i
+//     i=i+1
+// }
+// while(i<=num)
+// console.log("total of sum",sum)
+
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num==========")
+// var sum=0
+// var i=1
+// while(i<=num){
+//     sum=sum+i
+//     i=i+1
+// }
+// console.log("total of sum",sum)
+
+// do{
+//     sum=sum+i
+//     i=i+1
+// }
+// while(i<=num)
+// console.log("total of sum",sum)
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num===")
+// var i=0
+// while(i<=num){
+//     var num1 =readline.questionInt("enter the any num1======")
+//     if(num1>0){
+//         console.log(num1,"posstive")
+//     }
+//     else{
+//         console.log(num1,"neagtive")
+//     }
+//     i=i+1
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num=========")
+// var a=0;
+// var b=1;
+// console.log(a);
+// console.log(b);
+// for(var i=0; i<=num; i++){
+//     var temp =a+b
+//     console.log(temp)
+//     b=a
+//     a=temp
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num=======")
+// var fact=1
+// while(num>0){
+//     fact=fact*num
+//     num=num-1
+// }
+// console.log("fact",fact)
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num===========")
+// var num1 =readline.questionInt("enter the any num1=======")
+// while(num<=num1){
+//     var i=1
+//     while(i<=10){
+//         console.log(num*i)
+//         i=i+1
+//     }
+//     console.log(" ")
+//     num=num+1
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num=========")
+// var i=1
+// var sum=0
+// while(i<num){
+//     if(num%i==0){
+//         sum=sum+i
+        
+//     }
+//     i=i+1
+// }
+// if(sum==num){
+//     console.log(sum,"this is a perfect number")
+// }
+// else{
+//     console.log(sum,"this is a not perfect number")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num=========")
+// var i=2
+// var count=0
+// while(i<=num){
+//     if(num%i==0){
+//         count=count+1
+//     }
+//     i=i+1
+// }
+// console.log("count============",count)
+// if(count==1){
+//     console.log("this is a prime number")
+// }
+// else{
+//     console.log("this is a not prime number")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num==========")
+// var i=1
+// while(i<=num){
+//     if(i%3==0 && i%7==0){
+//         console.log(i,"navgurukul")
+//     }
+//     else if(i%7==0){
+//         console.log(i,"gurukul")
+//     }
+//     else if(i%3==0){
+//         console.log(i,"nav")
+//     }
+//     else{
+//         console.log(i)
+//     }
+//     i=i+1
+// }
